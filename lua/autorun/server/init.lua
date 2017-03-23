f382fbf39f54e87614193cde4e1ae8333d04df23
Servius <nosamrj@gmail.com>
2017-03-23 13:36:50 -0700

    Updated with all the stuff

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,5 +1,9 @@
 local function Plug( )
-print("Weapon Holstering Addon v0.1b")
-print("- Star Wars Packs currently in the works")
+print("Updated to Weapon Holstering v0.1c")
+print("Changelog:")
+print("- Version .1B->.1C")
+print("- Support for TFA Day of Defeat added.")
+print("- Support for Main Pack Pistols added.")
+print("Note: Star Wars weapons in the works. -Servius")
 end
 hook.Add("PlayerInitialSpawn", "Updates", Plug)
