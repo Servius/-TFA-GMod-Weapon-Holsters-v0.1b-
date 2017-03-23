877b23fb1a6b5bc3ecd1ec4ffad3e95993ebfef4
Servius <nosamrj@gmail.com>
2017-03-23 13:23:32 -0700

    Formatting

--- lua/autorun/server/init.lua
+++ lua/autorun/server/init.lua
@@ -1,5 +1,5 @@
 local function Plug( )
 print("Weapon Holstering Addon v0.1b")
-print("Star Wars Packs currently in the works")
+print("- Star Wars Packs currently in the works")
 end
 hook.Add("PlayerInitialSpawn", "Updates", Plug)
